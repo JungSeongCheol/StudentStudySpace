8b39866d4718da1e86fb8528acc35d868f021583
JungSeongCheol <kir5121@naver.com>
2020-01-31 15:32:11 +0900

    3장 내용

--- EsacpeTest/Main.c
+++ EsacpeTest/Main.c
@@ -11,12 +11,18 @@ int main()
 	printf("%lf\n", 3.4);
 	printf("%.1lf\n", 3.45);	//소수점 첫번째자리까지만 출력
 	printf("%.10lf\n", 3.4);
-	
 	printf("%d과 %d의 합은 %d입니다.\n", 10, 20, 10 + 20);
-
 	printf("%.1lf-%.1lf = %.1lf\n", 3.4, 1.2, 3.4 - 1.2);
 
-	// type here
+	printf("%.1lf\n", 1e6);
+	printf("%.7lf\n", 3.14e-5);
+	printf("%le\n", 0.0000314);
+	printf("%.2le\n", 0.0000314);
+
+	printf("%c\n", 'A');
+	printf("%s\n", "a");
+	printf("%c은 %s입니다.\n", '1', "first");
+	printf("%d\n", 'A');
 
 	system("pause");
 	return 0;
---
+++ ScanfTest/Main.c
@@ -0,0 +1,23 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif // _MSC_VER
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char grade;
+	char name[20];
+
+	// printf("나이 : %d, 키 : %lf", age, height);
+	
+	printf("학점을 입력하세요 :");
+	scanf("%c", &grade);
+	printf("이름을 입력하세요 :");
+	scanf("%s", name);
+	printf("학점은 %c, 이름은 %c입니다.", grade, name);
+	
+	system("pause");
+	return 0;
+}
