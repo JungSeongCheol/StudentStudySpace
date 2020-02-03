ca11238cb99986e8a349b779923423d8aaf3cc31
JungSeongCheol <kir5121@naver.com>
2020-02-03 17:31:39 +0900

    2/3 마지막 내용 추가

--- FileOpen/main.c
+++ FileOpen/main.c
@@ -9,24 +9,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <io.h>
-
-
-typedef struct _finddata_t FILE_SEARCH;
-
-void GetfileList(char* path);
+#include <sys/types.h>
+#include <string.h>
+#include <dirent.h>
+#include <conio.h>
+#include <errno.h>
 
 // 메인함수
+
+int txt_file_only(const struct dirent* info)
+{
+    char* onlyext;
+
+    onlyext = strrchr(info->d_name, '.');
+
+    if (onlyext == NULL)
+    {
+        return 1;
+    }
+
+    if (strcmp(onlyext, ".txt") == 0)
+        return 0;
+    else
+        return 1;
+}
 int main(void)
 {
-    char path[100] = "D:\\";
-    GetfileList(path);
+    FILE* fp;
+    int ch;
+    struct dirent** namelist;
+    int idx;
+    int count;
+
+    i
+
+    fp = fopen("d:\\.txt", "rt");
+
+
+    if (fp == NULL)
+    {
+        printf("파일이 열리지 않았습니다.\n");
+        return 1;
+    }
+
+    printf("\n");
+    fclose(fp);
 
     system("pause");
     return EXIT_SUCCESS;
 }
 
-void GetfileList(char* path)
-{
-    long h_file;
-    char search_Path[100];
-}
+
+
+//strrchr(paht, '/') 사용
