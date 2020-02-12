b4e0d5ed54179c10434070a5d48e3514f876aa37
JungSeongCheol <kir5121@naver.com>
2020-02-12 09:09:04 +0900

    CH20

--- UsingDynLibApp/main.c
+++ UsingDynLibApp/main.c
@@ -9,21 +9,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "mycalc.h"
+
+#pragma comment(lib, "MyMathLib.lib")
 
 // 메인함수
 int main(void) 
 {
-    int i = 0, j = 0;
-    char ch;
-    printf("정수를 입력하세요\n");
-    scanf("%d", &i);
+    int res = int_add(10, 30);
 
-    printf("수식(+, -, *, /)을 입력하세요\n");
-    scanf('c', ch);
+    printf("res = %d\n", res);
 
-    printf("정수를 입력하세요\n");
-    scanf("%d", &j);
-
+    res = int_multi(10, 40);
+    printf("res = %d\n", res);
 	system("pause");
 	return EXIT_SUCCESS;
 }
