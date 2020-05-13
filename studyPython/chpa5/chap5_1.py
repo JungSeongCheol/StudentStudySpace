d7e0c09d80ecdbd3d91c7ff74ff3dbc784088350
JungSeongCheol <kir5121@naver.com>
2020-05-13 16:24:17 +0900

    Chap5_2까지

--- studyPython/chpa5/chap5_1.py
+++ studyPython/chpa5/chap5_1.py
@@ -13,7 +13,7 @@ def print_3_times():
 #         print(value, end=" ")
 
 def print_n_times(*values, n=3):
-    for i in range(n):
+    for _ in range(n):
         for value in values:
             print(value, end=" ")
         print()
@@ -21,6 +21,16 @@ def print_n_times(*values, n=3):
 def func(a, b=10, c=20):
     print(a + b + c)
 
+def return_test():
+    print("Value A")
+    return 100
+    # print("Value B")
+
+def multi_all(start, end):
+    output = 1
+    for i in range(start, end + 1):
+        output *= i
+    return output
 
 # print_3_times()
 # print_n_times("Hello", n=4)
@@ -28,5 +38,16 @@ def func(a, b=10, c=20):
 # print_n_times("안녕하세요", n=1)
 # print_n_times("안녕하세요", "즐거운", "파이썬", "프로그래밍", n=1)
 
-func(10)
-func(c = 100, a = 50, b = 50) #쓸 수는 있지만 별로 좋진않음.
+# func(10)
+# func(c = 100, a = 50, b = 50) #쓸 수는 있지만 별로 좋진않음.
+
+# value = return_test()
+value = None
+# print("value 는 {}".format(value))
+
+if (value == None):
+    print("value는 값이 없습니다")
+else:
+    print("value 는 {}".format(value))
+
+print("1 to 100", multi_all(1, 100))
---
+++ studyPython/chpa5/chap5_2.py
@@ -0,0 +1,70 @@
+# def factorial(n):
+#     output = 1
+
+#     for i in range(1, n + 1):
+#         output *= i
+
+#     return output
+
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return n*factorial(n-1)
+    
+# def fibonacci(n):
+#     if n == 1:
+#         return 1
+#     if n == 2:
+#         return 1
+#     else:
+#         return fibonacci(n - 1) + fibonacci(n - 2)
+
+counter = 0
+import time
+
+# def fibonacci(n):
+    # print("fibonacci({})를 구합니다.".format(n))
+    # global counter
+    # counter += 1
+
+    # if n == 1:
+    #     return 1
+    # if n == 2:
+    #     return 2
+    # else:
+    #     return fibonacci(n-1) + fibonacci(n-2)
+
+dictionary = {
+    1: 1,
+    2: 2
+}
+
+def fibonacci(n):
+    if n in dictionary:
+        # 메모되어 있으면 메모된 값을 리턴
+        return dictionary[n]
+    else:
+        # 메모되어 있지 않으면 값을 구함
+        output = fibonacci(n-1) + fibonacci(n-2)
+        dictionary[n] = output
+        return output
+
+# print("1!: {}".format(factorial(1)))
+# print("2!: {}".format(factorial(2)))
+# print("3!: {}".format(factorial(3)))
+# print("4!: {}".format(factorial(4)))
+# print("5!: {}".format(factorial(5)))
+# print("10! : {}".format(format(factorial(10), ',d')))
+
+# print("fibonacci(1):", fibonacci(1))
+# print("fibonacci(2):", fibonacci(2))
+# print("fibonacci(3):", fibonacci(3))
+# print("fibonacci(4):", fibonacci(4))
+# print("fibonacci(5):", fibonacci(5))
+
+print("fibonnaci(1)", fibonacci(1))
+start = time.time()
+print("fibonnaci(1000)", fibonacci(1000) )
+print("실행시간 : ", time.time() - start, "sec")
+print("dict size {}".format(len(dictionary)))
