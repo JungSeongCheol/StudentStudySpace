374984217d3bb4b5a64e3fc278ba39e7845ddbf7
JungSeongCheol <kir5121@naver.com>
2020-07-03 17:04:28 +0900

    7월 3일 금요일 C# 스터디

---
+++ StudyCSharp/BoxingUnboxing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object a = 20;
+            int b = (int)a;
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+        }
+    }
+}
--- StudyCSharp/IntegerType/Program.cs
+++ StudyCSharp/IntegerType/Program.cs
@@ -6,25 +6,51 @@ namespace IntegerType
     {
         static void Main(string[] args)
         {
-            sbyte a = sbyte.MinValue;
-            byte b = byte.MaxValue;
+            //sbyte a = sbyte.MinValue;
+            //byte b = byte.MaxValue;
 
-            Console.WriteLine($"a = {a}, b = {b}");
+            //Console.WriteLine($"a = {a}, b = {b}");
 
-            short c = short.MinValue;
-            ushort d = ushort.MaxValue;
+            //short c = short.MinValue;
+            //ushort d = ushort.MaxValue;
 
-            Console.WriteLine($"c = {c}, d = {d}");
+            //Console.WriteLine($"c = {c}, d = {d}");
 
-            int e = int.MinValue;
-            uint f = uint.MaxValue;
+            //int e = int.MinValue;
+            //uint f = uint.MaxValue;
 
-            Console.WriteLine($"e = {e}, f = {f}");
+            //Console.WriteLine($"e = {e}, f = {f}");
 
-            long g = long.MinValue;
-            ulong h = 2_000_000_000_000_000_000;
+            //long g = long.MinValue;
+            //ulong h = 2_000_000_000_000_000_000;
 
-            Console.WriteLine($"g= {g}, h = {h}");
+            //Console.WriteLine($"g= {g}, h = {h}");
+
+            //byte a = 255;
+
+            //Console.WriteLine($"a = {a}");
+
+            //byte b = 0b1111_1111;
+            //Console.WriteLine($"b = {b}");
+
+            //byte c = 0xff;
+            //Console.WriteLine($"c = {c}");
+
+            //uint d = 0x1234_abcd;
+            //Console.WriteLine($"d = {d}");
+
+
+            byte d = byte.MaxValue;
+            Console.WriteLine($"d = {d}");
+            d++;
+            Console.WriteLine($"d = {d}");
+            d++;
+            Console.WriteLine($"d = {d}");
+
+            float f = float.MaxValue;
+//            double g = double.MaxValue;
+
+            Console.WriteLine($"f = {f}");
         }
     }
 }
