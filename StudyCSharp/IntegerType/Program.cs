84c6ef0bae85e7557835b941e1efc519033ed8be
JungSeongCheol <kir5121@naver.com>
2020-07-03 15:02:01 +0900

    IntegerType

---
+++ StudyCSharp/IntegerType/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IntegerType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte a = sbyte.MinValue;
+            byte b = byte.MaxValue;
+
+            Console.WriteLine($"a = {a}, b = {b}");
+
+            short c = short.MinValue;
+            ushort d = ushort.MaxValue;
+
+            Console.WriteLine($"c = {c}, d = {d}");
+
+            int e = int.MinValue;
+            uint f = uint.MaxValue;
+
+            Console.WriteLine($"e = {e}, f = {f}");
+
+            long g = long.MinValue;
+            ulong h = 2_000_000_000_000_000_000;
+
+            Console.WriteLine($"g= {g}, h = {h}");
+        }
+    }
+}
