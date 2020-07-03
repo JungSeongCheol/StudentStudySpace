f276c6b355db8a0052e2e41e26384e640994f40c
JungSeongCheol <kir5121@naver.com>
2020-07-03 14:26:37 +0900

    HelloApp 완료

--- StudyCSharp/HelloApp/Program.cs
+++ StudyCSharp/HelloApp/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Console;
 
 namespace HelloApp
 {
-    class Program
+    /// <summary>
+    /// 기본 클래스
+    /// </summary>
+     class Program
     {
+         /// <summary>
+         /// 엔트리포인트 메서드
+         /// </summary>
+         /// <param name="args"> </param>
+
+        
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                WriteLine("ex: helloapp.exe <이름>");
+                return;
+            }
+
+            else
+                WriteLine($"Hello, {args[0]}!");
         }
     }
 }
