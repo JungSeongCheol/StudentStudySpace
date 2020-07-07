fb30a62c0f4d973fcf0ea02bd4b998865b73f788
JungSeongCheol <kir5121@naver.com>
2020-07-07 12:24:05 +0900

    Chap07 Class

---
+++ StudyCSharp/BasicClass/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClass
+{
+    class Cat
+    {
+        public string Name;
+        public Color Color;
+
+        public Cat()
+        {
+            Name = "";
+            Color = Color.Transparent;
+        }
+
+        /// <summary>
+        /// 파라미터 생성자
+        /// </summary>
+        /// <param name="name">고양이 이름</param>
+        /// <param name="color">고양이 털색</param>
+        public Cat(string name, Color color)
+        {
+            Name = name;
+            Color = color;
+        }
+        ~Cat()
+        {
+            Console.WriteLine("Distruct!");
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine($"{Name}, 야옹~!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat Kitty = new Cat(); // 인스턴스 화
+            Kitty.Name = "키티";
+            Kitty.Color = Color.White;
+            Kitty.Meow();
+            Console.WriteLine($"{Kitty.Name} : {Kitty.Color}");
+
+            Cat nero = new Cat("네로", Color.Black);
+            nero.Meow();
+            Console.WriteLine($"{nero.Name} : {nero.Color}");
+
+        }
+    }
+}
--- StudyCSharp/Ex6_2/Program.cs
+++ StudyCSharp/Ex6_2/Program.cs
@@ -13,13 +13,13 @@ namespace Ex6_2
         {
             double mean = 0;
 
-            Mean(1, 2, 3, 4, 5, ref mean);
+            Mean(1, 2, 3, 4, 5, out mean);
 
             Console.WriteLine($"평균 {mean}");
             
         }
 
-        public static void Mean(double a, double b, double c, double d, double e, ref double mean)
+        public static void Mean(double a, double b, double c, double d, double e, out double mean)
         {
             mean = (a + b + c + d + e) / 5;
         }
