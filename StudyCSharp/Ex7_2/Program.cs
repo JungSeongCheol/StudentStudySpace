6412b013ea4b8d285725574744716ed842edc023
JungSeongCheol <kir5121@naver.com>
2020-07-08 09:18:10 +0900

    7/7 Study Class

---
+++ StudyCSharp/Ex7_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex7_2
+{
+    class A
+    {
+
+    }
+
+    class B : A
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            B b = new B();
+            A c = new B();
+          //  B d = new A();
+        }
+    }
+}
---
+++ StudyCSharp/NestedClass/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedClass
+{
+    class Configuration
+    {
+        List<ItemValue> listConfig = new List<ItemValue>();
+
+        public void SetConfig(string item, string value)
+        {
+            ItemValue iv = new ItemValue();
+            iv.SetValue(this, item, value);
+        }
+
+        public string GetConfig(string item)
+        {
+            foreach (ItemValue iv in listConfig)
+            {
+                if(iv.GetItem() == item)
+                    return iv.GetValue();   //item에 속해있던 value만 나오게 된다.
+   
+            }
+
+            return "";
+        }
+
+        private class ItemValue
+        {
+            private string item;
+            private string value;
+
+            public void SetValue(Configuration config, string item, string value) //들어온 매개변수인 item이 같으면 add시키지 않고 원래 item과 value에 들어온 값을 덮어버리고, 아니면 item과 값을 등록한다.
+            {
+                this.item = item;
+                this.value = value;
+
+                bool found = false;
+                for (int i = 0; i < config.listConfig.Count; i++)
+                {
+                    if (config.listConfig[i].item == item)
+                    {
+                        config.listConfig[i] = this;
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (found == false)
+                {
+                    config.listConfig.Add(this);
+                }
+            }
+
+            public string GetItem() { return item; }
+            public string GetValue() { return value; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Configuration config = new Configuration();
+            config.SetConfig("Version", "V 5.0");
+            config.SetConfig("size", "655,324 KB");
+
+            Console.WriteLine(config.GetConfig("Version"));
+            Console.WriteLine(config.GetConfig("size"));
+
+            config.SetConfig("Version", "V 5.0.1");
+            Console.WriteLine(config.GetConfig("Version"));
+        }
+    }
+
+
+}
